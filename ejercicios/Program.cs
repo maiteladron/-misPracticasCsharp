dab4a48a7b8de941f7fbf89d9e781284c7e5b70b
maiteladron <maite_ladron@hotmail.es>
2017-07-21 12:24:33 +0200

    prueba de cambios en el programa

--- ejercicios/Program.cs
+++ ejercicios/Program.cs
@@ -151,6 +151,7 @@ namespace ejercicios
                 {
                     Console.WriteLine("APROBADO.");
                 }
+                //Esto  es una prueba para ver que funciona el GIT
 
             }
             catch
